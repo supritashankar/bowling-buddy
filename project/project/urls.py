6a5fc4f6022a75a938f4a8ac1b4043e56d5a79ab
Suprita Shankar <suprita.shankar@gmail.com>
2014-04-01 17:06:53 -0400

    application to process data

--- project/project/urls.py
+++ project/project/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^server/', include(server.urls),
 )
