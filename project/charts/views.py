a75db531443b37249db349f717be579816eb33e9
Suprita Shankar <suprita.shankar@gmail.com>
2014-04-07 13:22:39 -0400

    use awesome charts

---
+++ project/charts/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from chartit import DataPool, Chart
+
+def weather_chart_view(request):
+    #Step 1: Create a DataPool with the data we want to retrieve.
+    weatherdata = \
+        DataPool(
+           series=
+            [{'options': {
+               'source': MonthlyWeatherByCity.objects.all()},
+              'terms': [
+                'month',
+                'houston_temp',
+                'boston_temp']}
+             ])
+
+    #Step 2: Create the Chart object
+    cht = Chart(
+            datasource = weatherdata,
+            series_options =
+              [{'options':{
+                  'type': 'line',
+                  'stacking': False},
+                'terms':{
+                  'month': [
+                    'boston_temp',
+                    'houston_temp']
+                  }}],
+            chart_options =
+              {'title': {
+                   'text': 'Weather Data of Boston and Houston'},
+               'xAxis': {
+                    'title': {
+                       'text': 'Month number'}}})
+
+    #Step 3: Send the chart object to the template.
+    'weatherchart': cht
+    return render_to_response('charts/demo.html', locals(), RequestContext(request))
