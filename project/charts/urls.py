4a57f24ba06972bb863dfb357233f1728e8998f5
Suprita Shankar <suprita.shankar@gmail.com>
2014-04-08 13:49:11 -0400

    implement another chart

--- project/charts/urls.py
+++ project/charts/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('charts.views',
     url(r'^demo/$','demo'),
+    url(r'^demo1/$', 'demo1'),
 )
--- project/charts/views.py
+++ project/charts/views.py
@@ -5,6 +5,36 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from charts.models import *
 
+def demo1(request):
+  ds = DataPool(
+       series=
+        [{'options': {
+            'source': MonthlyWeatherByCity.objects.all()},
+          'terms': [
+            'month',
+            'houston_temp', 
+            'boston_temp']}
+         ])
+
+  cht = Chart(
+        datasource = ds, 
+        series_options = 
+          [{'options':{
+              'type': 'column',
+              'stacking': True},
+            'terms':{
+              'month': [
+                'boston_temp',
+                'houston_temp']
+              }}],
+        chart_options = 
+          {'title': {
+               'text': 'Weather Data of Boston and Houston'},
+           'xAxis': {
+                'title': {
+                   'text': 'Month number'}}})
+  return render_to_response('charts/demo.html', {'weatherchart':cht})
+
 def demo(request):
     #Step 1: Create a DataPool with the data we want to retrieve.
     weatherdata = \
