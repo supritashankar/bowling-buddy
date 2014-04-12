17c48d3e5783de9cba1f7fd596994d9f24ec1b6a
Suprita Shankar <suprita.shankar@gmail.com>
2014-04-12 18:02:30 -0400

    add the templates

--- project/plotdata/views.py
+++ project/plotdata/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+
+def homepage(request):
+
+  """ Homepage which will display all the the different times of bowls """
+
+  return render_to_response('plotdata/homepage.html')
