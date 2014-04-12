428918cf5e60022b58341346166a94e6405bae10
Suprita Shankar <suprita.shankar@gmail.com>
2014-04-12 18:11:55 -0400

    define models to store data

--- project/plotdata/admin.py
+++ project/plotdata/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from plotdata.models import BowlingData
 
-# Register your models here.
+admin.site.register(BowlingData)
--- project/plotdata/models.py
+++ project/plotdata/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class BowlingData(models.Model):
+  """ Defines the model of the data values to be displayed """
+  
+  xvalue = models.CharField(max_length = 20)
+  yvalue = models.CharField(max_length = 20)
+  zvalue = models.CharField(max_length = 20)
+  angle   = models.CharField(max_length = 10)
+  accleration = models.CharField(max_length = 10)
+  time 	  = models.CharField(max_length = 10)
+
+  def __unicode__(self):
+    return "This is the values for this " + str(self.id)
