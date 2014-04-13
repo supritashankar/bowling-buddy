5a360610ab4bd62a75c1e72f5185cb650f9e5da9
a-rey <aaronreyes2@gmail.com>
2014-04-12 20:38:28 -0400

    working code for getting 5 swings to SD card. fail-safe mode/thresholds needed still

--- arduino/playground/playground.ino
+++ arduino/playground/playground.ino
@@ -1,63 +1,33 @@
+// todo
+// fix Vcc wire on test teensy
+// figure out how to make teensy fail safe (how to recover from error? maybe reset button)
+// how many swings in a game of bowling
+// blinking #number of errors?
+// error if SD card size < size needed? (need to get this threshhold)
 
+#include <SD.h>
 #include "Wire.h"
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
+ 
+#define LED_PIN      11   // pin of on-board LED
+#define BUTTON_PIN   17   // the record button
+#define RECORD_LIMIT 5000 // time in milliseconds for swing record time
+#define MAX_SWINGS   5    // how many swings we will record on the SD card
 
-MPU6050 mpu; // default I2C address is 0x68
+MPU6050 mpu;
 
-// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
-// quaternion components in a [w, x, y, z] format (not best for parsing
-// on a remote host such as Processing or something though)
-#define OUTPUT_READABLE_QUATERNION
+int button_state = 0;         // current state of the button
+int last_button_state = 0;    // previous state of the button
 
-// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
-// binary, one right after the other. This is very fast (as fast as possible
-// without compression or data loss), and easy to parse, but impossible to read
-// for a human.
-//#define OUTPUT_BINARY_ACCELGYRO
+bool error = false; // used to signify error
 
-// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
-// (in degrees) calculated from the quaternions coming from the FIFO.
-// Note that Euler angles suffer from gimbal lock (for more info, see
-// http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_EULER
+uint8_t  swing_count = 1; // keeps track of which swing we are on
+unsigned long record_time;
+File     results;
 
-// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
-// pitch/roll angles (in degrees) calculated from the quaternions coming
-// from the FIFO. Note this also requires gravity vector calculations.
-// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
-// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_YAWPITCHROLL
-
-// uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
-// components with gravity removed. This acceleration reference frame is
-// not compensated for orientation, so +X is always +X according to the
-// sensor, just without the effects of gravity. If you want acceleration
-// compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
-//#define OUTPUT_READABLE_REALACCEL
-
-// uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
-// components with gravity removed and adjusted for the world frame of
-// reference (yaw is relative to initial orientation, since no magnetometer
-// is present in this case). Could be quite handy in some cases.
-//#define OUTPUT_READABLE_WORLDACCEL
-
-/* =========================================================================
-   Remark: on the Teensy2.0 and Teensy2.0++ interrupt #0 is the same pin as
-   one of SDA or SCL - and apparently it works without connecting the INT pin 
-   on the MPU-9150 breakout board??
-   (I even tested it on the UNO: also works without this pin connected?)
-   Later I realized that i can set another interrupt, e.g. interupt 2 on the Teensy2.0
-   (= pin 7 on the Teensy2.0).
- * ========================================================================= */
-
-#define LED_PIN    11 // pin of on-board LED
-#define BUTTON_PIN 3  // the record button
-
-bool blinkState = false;
-
+bool record = false; // for when to record swing
 // MPU control/status vars
-bool dmp_ready = false;  // set true if DMP init was successful
 uint8_t  mpu_interrupt_status;   // holds actual interrupt status byte from MPU
 uint8_t  device_status;      // return status after each device operation (0 = success, !0 = error)
 uint16_t packet_size;    // expected DMP packet size (default is 42 bytes)
@@ -66,79 +36,90 @@ uint8_t  fifo_buffer[64]; // FIFO storage buffer
 
 // orientation/motion vars
 Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
+//VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+//VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+//VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+//VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+//float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpu_interrupt = false; // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpu_interrupt = true;
-}
 
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
 
 void setup() {
-    // join I2C bus
-    Wire.begin();
-
-    // initialize serial communication
-    Serial.begin(57600); 
-    
-    // initialize device
-    Serial.println(F("Initializing I2C devices..."));
-    mpu.initialize();
-
-    // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // wait for ready
-    delay(10000); 
-
-    // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
-    device_status = mpu.dmpInitialize();
-    
-    // make sure it worked (returns 0 if so)
-    if (device_status == 0) {
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
-
-        // enable Arduino interrupt detection
-        //Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        //attachInterrupt(0, dmpDataReady, RISING);    // (Gidi) if you use the interrupts, you can set this to interrupt 2 for the Teensy2.0 
-        mpu_interrupt_status = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmp_ready = true;
-
-        // get expected DMP packet size for later comparison
-        packet_size = mpu.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(device_status);
-        Serial.println(F(")"));
+  // configure error LED for output
+  pinMode(LED_PIN, OUTPUT);
+  // configure an actual input for a button
+  pinMode(BUTTON_PIN, INPUT);
+  // necessary for SD library to work
+  pinMode(10, OUTPUT);
+  
+  Wire.begin();
+  Serial.begin(57600);
+  Serial.println(F("~~~~~~~~~~~~ I2C SETUP ~~~~~~~~~~~~~~~~"));
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  delay(2000); // wait for ready
+  Serial.println(F("~~~~~~~~~~~ ACCEL SETUP ~~~~~~~~~~~~~~~"));
+  Serial.println(F("Initializing DMP..."));
+  device_status = mpu.dmpInitialize();
+  // make sure it worked (returns 0 if so)
+  if (device_status == 0) {
+      Serial.println(F("Enabling DMP..."));
+      mpu.setDMPEnabled(true);
+      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+      // get expected DMP packet size for later comparison
+      packet_size = mpu.dmpGetFIFOPacketSize();
+  } else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(device_status);
+      Serial.println(F(")"));
+      error = true;
+  }
+  Serial.println(F("~~~~~~~~~~~~~ SD SETUP ~~~~~~~~~~~~~~~~"));
+  // if we have not had an error yet, continue to SD setup
+  if (!error) {
+    // hang and blink LED to convey SD insertion
+    while (true) {
+      if (!SD.begin(0)) {
+        Serial.println("Insert SD card.");
+        if (!error) {
+          digitalWrite(LED_PIN, HIGH); 
+          error = true;
+          delay(500);
+        } else {
+          digitalWrite(LED_PIN, LOW); 
+          error = false;
+          delay(500);
+        }
+      } else {
+        break;
+      }
     }
-
-    // configure LED for output
-    pinMode(LED_PIN, OUTPUT);
-    // configure an actual input for a button
-    pinMode(BUTTON_PIN, INPUT);
+    // remove old files
+    SD.remove("1.TXT"); SD.remove("2.TXT");
+    SD.remove("3.TXT"); SD.remove("4.TXT");
+    SD.remove("5.TXT");
+    Serial.println("SD card initialization done.");
+    results = SD.open("1.txt", FILE_WRITE); // first swing
+    if (!results) {
+      Serial.println("error opening file on SD card!");
+      error = true;
+    } else {
+      error = false; 
+    }
+  } else {
+    Serial.println("SD card cannot start due to previous errors.");
+  }
+  Serial.println(F("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"));
 }
 
 // ================================================================
@@ -146,121 +127,119 @@ void setup() {
 // ================================================================
 
 void loop() {
-    // if programming failed, don't try to do anything
-    if (!dmp_ready) return;
+  // if setup failed, turn on error LED and exit
+  if (error) {
+      digitalWrite(LED_PIN, HIGH);
+      return;
+  } else {
+      digitalWrite(LED_PIN, LOW);
+  }
+  
+  // read from button pin
+  button_state = digitalRead(BUTTON_PIN);
 
-    // wait for MPU interrupt or extra packet(s) available
-    //while (!mpuInterrupt && fifoCount < packetSize) {
-        // other program behavior stuff here
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
-        // Gidi: N.B. I noticed that the program never actually gets into this
-        // while loop (because I did not connect the interrupt pin ;-)), 
-        // so I placed my code outside of this loop.
-        // Later I tested it on the Uno and on the Teensy2.0 with the interrupt pin
-        // attached, and I was able to put my communication code inside this loop
-    //}
-
-    // reset interrupt flag and get INT_STATUS byte
-    mpu_interrupt = false;
-    mpu_interrupt_status = mpu.getIntStatus();
-
-    // get current FIFO count
-    fifo_count = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpu_interrupt_status & 0x10) || fifo_count == 1024) {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpu_interrupt_status & 0x02) {
+  // compare the button State to its previous state if not recording
+  if (!record) {
+   if (button_state != last_button_state) {
+      if (button_state == HIGH) {
+        // set flag and get start of swing recording
+        Serial.println(F("RECORDING..."));
+        record = true;
+        record_time = millis();
+      } 
+    }
+    // save the current state as the last state
+    last_button_state = button_state; 
+  } else {
+    // see if we are done recording
+    if ((millis() - record_time) >= RECORD_LIMIT) {
+      record = false;
+      Serial.println(F("DONE RECORDING"));
+      swing_count++; // make next swing
+      // close SD card file
+      results.close();
+      // try to open another file on SD card
+      if (swing_count > MAX_SWINGS) {
+        error = true; 
+        return; // exit CALL setup() !?!?!?!?!?
+      } else {
+        switch (swing_count) {
+          case 2:
+            results = SD.open("2.txt", FILE_WRITE);
+            break;
+          case 3:
+            results = SD.open("3.txt", FILE_WRITE);
+            break;
+          case 4:
+            results = SD.open("4.txt", FILE_WRITE);
+            break;
+          case 5:
+            results = SD.open("5.txt", FILE_WRITE);
+            break;
+        }
+        if (!results) {
+          error = true;
+          return; // exit
+        }
+      }
+    // else, we are recording!
+    } else {
+      // get INT_STATUS byte
+      mpu_interrupt_status = mpu.getIntStatus();
+      // get current FIFO count
+      fifo_count = mpu.getFIFOCount();
+      // check for overflow (this should never happen unless our code is too inefficient)
+      if ((mpu_interrupt_status & 0x10) || fifo_count == 1024) {
+          // reset so we can continue cleanly
+          mpu.resetFIFO();
+      // otherwise, check for DMP data ready interrupt (this should happen frequently)
+      } else if (mpu_interrupt_status & 0x02) {
         // wait for correct available data length, should be a VERY short wait
         while (fifo_count < packet_size) {
           fifo_count = mpu.getFIFOCount();
         }
-
         // read a packet from FIFO
         mpu.getFIFOBytes(fifo_buffer, packet_size);
-        
         // track FIFO count here in case there is > 1 packet available
         // (this lets us immediately read more without waiting for an interrupt)
         fifo_count -= packet_size;
-
-        #ifdef OUTPUT_READABLE_QUATERNION
-            // display quaternion values in easy matrix form: w x y z
-            mpu.dmpGetQuaternion(&q, fifo_buffer);
-            Serial.print("quat\t");
-            Serial.print(q.w);
-            Serial.print("\t");
-            Serial.print(q.x);
-            Serial.print("\t");
-            Serial.print(q.y);
-            Serial.print("\t");
-            Serial.println(q.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_EULER
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifo_buffer);
-            mpu.dmpGetEuler(euler, &q);
-            Serial.print("euler\t");
-            Serial.print(euler[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(euler[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(euler[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_YAWPITCHROLL
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            Serial.print("ypr\t");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(ypr[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_REALACCEL
-            // display real acceleration, adjusted to remove gravity
-            mpu.dmpGetQuaternion(&q, fifo_buffer);
-            mpu.dmpGetAccel(&aa, fifo_buffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            Serial.print("areal\t");
-            Serial.print(aaReal.x);
-            Serial.print("\t");
-            Serial.print(aaReal.y);
-            Serial.print("\t");
-            Serial.println(aaReal.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_WORLDACCEL
-            // display initial world-frame acceleration, adjusted to remove gravity
-            // and rotated based on known orientation from quaternion
-            mpu.dmpGetQuaternion(&q, fifo_buffer);
-            mpu.dmpGetAccel(&aa, fifo_buffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-            Serial.print("aworld\t");
-            Serial.print(aaWorld.x);
-            Serial.print("\t");
-            Serial.print(aaWorld.y);
-            Serial.print("\t");
-            Serial.println(aaWorld.z);
-        #endif
-
-        // blink LED to indicate activity
-        blinkState = !blinkState;
-        digitalWrite(LED_PIN, blinkState);
+        // display quaternion values in easy matrix form: x y z
+        mpu.dmpGetQuaternion(&q, fifo_buffer);
+        // display initial world-frame acceleration, adjusted to remove gravity
+        // and rotated based on known orientation from quaternion
+        //mpu.dmpGetAccel(&aa, fifo_buffer);
+        //mpu.dmpGetGravity(&gravity, &q);
+        //mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+        // display Euler angles in degrees!
+        mpu.dmpGetEuler(euler, &q);
+        // make a string for assembling the data to log:
+        String data_string = "";
+        // 1) add time since program running (in milliseconds)
+        data_string += String(millis());
+        data_string += ",";
+        // 2) x, y, and z
+        data_string += String(q.x);
+        data_string += ",";
+        data_string += String(q.y);
+        data_string += ",";
+        data_string += String(q.z);
+        data_string += ",";
+        // 3) acceleration in x, y, and z direction
+       // data_string += String(aaWorld.x);
+       // data_string += ",";
+       // data_string += String(aaWorld.y);
+       // data_string += ",";
+       // data_string += String(aaWorld.z);
+       // data_string += ",";
+        // 4) euler angles (in degrees) [psi, theta, phi]
+        data_string += String(euler[0] * 180/M_PI);
+        data_string += ",";
+        data_string += String(euler[1] * 180/M_PI);
+        data_string += ",";
+        data_string += String(euler[2] * 180/M_PI);
+        // write data to SD card file
+        results.println(data_string);
+      }
     }
+  }
 }
-
