173e3b3874e1618998d56349f51d18dc938a4a38
Suprita Shankar <suprita.shankar@gmail.com>
2014-04-15 19:46:01 -0400

    tidy

--- project/project/settings.py
+++ project/project/settings.py
@@ -4,6 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 PROJECT_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'n^5(h87h-alb8@0uyp*ptb$p&4vp$$8hz&38szwhv@=78umr18'
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -12,9 +13,6 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -46,8 +44,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -58,9 +54,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
