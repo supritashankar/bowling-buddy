572100b78fa48b4a655350811049ec10fd6694f3
Suprita Shankar <suprita.shankar@gmail.com>
2014-04-14 21:44:29 -0400

    for #1

--- project/plotdata/views.py
+++ project/plotdata/views.py
@@ -7,3 +7,10 @@ def homepage(request):
 
   """ Homepage which will display all the the different times of bowls """
   return render_to_response('plotdata/homepage.html')
+
+def data(request, round):
+
+  """ Data that will display results of a particular round """
+  """ For SD card on Desktop f = open("/Volumes/NO\ NAME/1.txt", "r") """
+
+  return render_to_response('plotdata/data.html')
