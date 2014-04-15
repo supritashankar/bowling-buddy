0593e3a2635c5e762f7d0de0301d254721e23c42
Suprita Shankar <suprita.shankar@gmail.com>
2014-04-15 00:30:43 -0400

    yay charts

--- project/plotdata/views.py
+++ project/plotdata/views.py
@@ -27,10 +27,38 @@ def data(request, round):
         zval         = line.split(',')[2]
         twist 	     = line.split(',')[3]
         bend         = line.split(',')[4]
-        
+       
         BowlingData.objects.create(time_elapsed = time_elapsed, xvalue = xval, yvalue = yval, zvalue = zval, twist = twist, bend = bend)
-
 
   print 'Created objects successfully'
   print len(BowlingData.objects.all())
-  return render_to_response('plotdata/data.html')
+
+  bowlingdata = \
+        DataPool(
+           series=
+            [{'options': {
+               'source': BowlingData.objects.all()},
+              'terms': [
+                'time_elapsed',
+                'xvalue',
+                'yvalue']}
+             ])
+
+  cht = Chart(
+            datasource = bowlingdata,
+            series_options =
+              [{'options':{
+                  'type': 'line',
+                  'stacking': False},
+                'terms':{
+                  'time_elapsed': [
+                    'xvalue',
+                    'yvalue']
+                  }}],
+            chart_options =
+              {'title': {
+                   'text': 'Bowling Data of x-axis and y-axis'},
+               'xAxis': {
+                    'title': {
+                       'text': 'Time elapsed'}}}) 
+  return render_to_response('plotdata/data.html', {'datachart':cht})
