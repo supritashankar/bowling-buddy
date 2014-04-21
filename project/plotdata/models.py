b467fc0805354313d12b91e3625370682cccd256
Suprita Shankar <suprita.shankar@gmail.com>
2014-04-20 21:51:04 -0400

    changes to model

--- project/plotdata/models.py
+++ project/plotdata/models.py
@@ -3,14 +3,22 @@ from django.db import models
 class BowlingData(models.Model):
   """ Defines the model of the data values to be displayed """
 
-  time_elapsed = models.IntegerField()
-  xvalue       = models.IntegerField()
-  yvalue       = models.IntegerField()
-  zvalue       = models.IntegerField()
-  twist        = models.DecimalField(max_digits = 9, decimal_places = 3)
-  bend         = models.DecimalField(max_digits = 9, decimal_places = 3)
-  created      = models.DateTimeField(auto_now_add=True)
-  frame_num    = models.CharField(max_length = 2)
+  timetaken        = models.IntegerField()
+  twist            = models.DecimalField(max_digits = 9, decimal_places = 3)
+  bend             = models.DecimalField(max_digits = 9, decimal_places = 3)
+  frame_num        = models.CharField(max_length = 2)
+  velocity         = models.ManyToManyField(InstantaenousVelocity)
+  average_velocity = models.DecimalField(max_digits = 9, decimal_places = 3)
+  created          = models.DateField(auto_now_add=True)
 
   def __unicode__(self):
     return "This is the values for this " + str(self.id)
+
+class InstantaenousVelocity(models.Model):
+
+  """ For a given frame define the instantaenous velocity """
+
+  xvalue        = models.IntegerField()
+  yvalue        = models.IntegerField()
+  zvalue        = models.IntegerField()
+  time_interval = models.IntegerField()
